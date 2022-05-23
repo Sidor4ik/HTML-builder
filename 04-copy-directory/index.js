3481c31d6e8afb7917664ffa242cdd364679b03a
Sidor4ik <57646827+Sidor4ik@users.noreply.github.com>
2022-05-23 20:34:15 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const stream = path.join(__dirname, 'files');
+const stream_copy = path.join(__dirname, 'files-copy');
+console.log(stream, '\n', stream_copy);
 
+
+function copyDir() {
+	fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+		if (err) throw err;
+		console.log('"files-copy" создана');
+	});
+
+
+	fs.readdir(stream_copy, (error, data) => {
+		if (error) return console.error(error.message);
+		data.forEach(element => {
+			fs.unlink(path.join(__dirname, 'files-copy', element), err => {
+				if (err) return console.error(err.message);
+			});
+		});
+	});
+
+	fs.readdir(stream, (error, data) => {
+		if (error) return console.error(error.message);
+		data.forEach(element => {
+			fs.copyFile(path.join(__dirname, 'files', element), path.join(__dirname, 'files-copy', element),
+				err => {
+					if (err) return console.error(err.message);
+				});
+		});
+	});
+}
+copyDir();
